e049235195b181f44f25acc69dd9f9bd6382d98c
eyal gabizon <eygabi01@gmail.com>
2023-07-22 15:05:55 +0300

    revert

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_WPXMmXYeq',
-        userPoolClientId: '5u44j8eqhtjvouo4ve7u2rskb1', 
-        region: 'us-east-1'
+        userPoolId: '',
+        userPoolClientId: '', 
+        region: ''
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
