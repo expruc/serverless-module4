e9ae93fb3bd652f50dc448ba88dc54186d402379
eyal gabizon <eygabi01@gmail.com>
2023-07-29 07:13:36 +0300

    added cognito

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '',
-        userPoolClientId: '', 
-        region: ''
+        userPoolId: 'us-east-1_o6hZQrH5A',
+        userPoolClientId: '2ajuceqhhhr1d0i2nk7e5p3fu5', 
+        region: 'us-east-1'
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://master.d2ixwyhm7ddr3o.amplifyapp.com/' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
